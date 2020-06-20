d3efaf461db04d8084fa111177bb35d3f50e3786
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-06-20 17:34:36 +0700

    init

---
+++ src/TwitterTwoExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RifqiSah\TwitterTwo;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class TwitterTwoExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'twittertwo', __NAMESPACE__.'\Provider', __NAMESPACE__.'\Server'
+        );
+    }
+}
